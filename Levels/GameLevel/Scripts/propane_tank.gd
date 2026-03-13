5e334196a26b384ae627a1a3569f23acf5e6c4f8
MyNamesRubber <mullenissiah14@gmail.com>
2026-03-13 13:58:06 -0500

    0.41 - General fixing/additions

--- Levels/GameLevel/Scripts/propane_tank.gd
+++ Levels/GameLevel/Scripts/propane_tank.gd
@@ -5,8 +5,12 @@ extends Node3D
 func _ready() -> void:
 	#$AnimationPlayer.play("RESET")
 	$AnimationPlayer.play("movement")
-
+	$AudioStreamPlayer3D.play()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
 		body.load_check()
+
+# Play audio when it stops
+func _on_audio_stream_player_3d_finished() -> void:
+	$AudioStreamPlayer3D.play()
